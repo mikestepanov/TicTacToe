58b431c75f9bd7ff7c167ac41a3aa12ac895bbaf
mikestepanov <mikestepanov777@gmail.com>
2018-02-20 17:57:23 -0800

    basic styling

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const game = document.querySelector('#game');
 let current = 'X';
+var board = [['', '', ''], ['', '', ''], ['', '', '']];
 
 game.addEventListener('click', (event) => {
-  if (event.target.tagName === 'LI') {
+  if (event.target.tagName === 'LI' && event.target.innerHTML === '') {
+    event.target.innerHTML = current;
+    event.target.style.verticalAlign = "middle";
+    let ans = gameLogic();
+    if (ans) {
 
-    switchPlayer();
+    } else {
+      switchPlayer();
+    }
   }
 });
+
+
+const gameLogic = () => {
+
+};
+
+
 
 const switchPlayer = () => {
   if (current === 'X') {
